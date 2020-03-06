b4aa2bd71269560fd808bceb96e7caf23477e843
Guy <israelonion@gmail.com>
2020-03-06 20:58:32 +0200

    Create a minimal website

---
+++ frontend/src/components/main/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="main-container">
+      <div className="healthbar-container">
+        <h1 id="title">
+          HP
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
