a02967e26030880911cb3b20c7b373f4945d93c7
Roynecro97 <Roynecro97@users.noreply.github.com>
2020-03-08 23:52:02 +0200

    Added a thread for the healthbar.

--- backend/healthbar/base.py
+++ backend/healthbar/base.py
@@ -4,20 +4,27 @@ Simple health bar implementation.
 
 Implements a base for an animated health bar logic.
 '''
+import threading
+
 from abc import ABCMeta, abstractmethod
+from time import sleep
 from typing import Union
 
 class HealthBarBase(metaclass=ABCMeta):
     '''
     Base class for health bar implementaions.
     '''
-    def __init__(self, length:int, max_value: Union[int, float]=100, *, start_at: Union[int, float]=None):
+    def __init__(self, length: int, max_value: Union[int, float] = 100, *, start_at: Union[int, float] = None,
+                 interval: float = 0.1):
         self.__length = length
         self.__max_value = max_value
         if start_at is None:
             self.__value = max_value
         else:
-            self.__value = start_at
+            self.value = start_at
+        self.__thread = None
+        self.__interval = interval
+        self.__run = False
 
     @property
     def length(self):
@@ -48,12 +55,67 @@ class HealthBarBase(metaclass=ABCMeta):
         '''
         self.__value = max(0, min(value, self.max_value))
 
+    def set_value(self, value):
+        '''
+        Sets the value for the current health bar.
+        This is the prefered method as it can be overridden by implementing classes.
+        Uses the self.value property.
+        '''
+        self.value = value
+
+    @property
+    def interval(self):
+        '''
+        The refresh interval of the health bar's thread.
+        '''
+        return self.__interval
+
+    def is_alive(self):
+        '''
+        Check if the drawing thread is alive.
+        '''
+        return self.__thread is not None and self.__thread.is_alive()
+
+    def start(self):
+        '''
+        Starts the thread. If it is already running, does nothing.
+        '''
+        if self.is_alive():
+            return
+        self.__run = True
+        self.__thread = threading.Thread(target=self.__loop)
+        self.__thread.start()
+
+    def stop(self):
+        '''
+        Stops the thread.
+        '''
+        if self.is_alive():
+            self.__run = False
+            self.__thread.join()
+
+    def in_drawing_thread(self):
+        '''
+        Checks if the current thread is the drawing thread.
+        This is meant for implementing healthbars to be able to check whether the draw method
+        is called in the drawing thread or called manually.
+        '''
+        return threading.current_thread() is self.__thread
+
     @abstractmethod
     def draw(self):
         '''
         Visually display the health bar.
         '''
-        raise NotImplemented
+        raise NotImplementedError
+
+    def __loop(self):
+        '''
+        Run the loop for the drawing thread.
+        '''
+        while self.__run:
+            self.draw()
+            sleep(self.interval)
 
 
 class ConsoleHealthBar(HealthBarBase):
